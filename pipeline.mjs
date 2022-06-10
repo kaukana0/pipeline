fbdd3886dbcb27493e0d06b38940951c2c42a09b
robert <kaukana0@tuta.io>
2022-06-10 10:49:38 +0200

    hook to access (r/w) raw data

--- pipeline.mjs
+++ pipeline.mjs
@@ -8,21 +8,30 @@
  */
 
 
-// the actual implementation which wires the pipeline together
-export function run(processingCfg, callback) {
+function arrayBuffer2Json(arrayBuffer) {
+    var dataView = new DataView(arrayBuffer);
+    var decoder = new TextDecoder('utf8');
+    var obj = JSON.parse(decoder.decode(dataView));
+    return obj
+}
+
+// the actual implementation which wires the pipeline together.
+// the hook provides a means to manipulate raw data - it's return value doesn't neccessarily have to be a JSON object.
+export function run(processingCfg, callback, hook=arrayBuffer2Json) {
     let output = {}
     Promise
     .all( processingCfg.map(el => { return fetch(el.input) }) )     // fetch data; only continue happy path when all requests are successfully finished
     .then(responses => {
         Promise
-        .all( responses.map(response => { return response.json() }) )   // get json from it
-        .then(jsonObjs => { 
+        .all( responses.map(response => { return response.arrayBuffer() }) )   // get json from it
+        .then(arrBuffs => { 
             // let a chain of functions do sth with it (i.e. "process" it).
             // this follows a cumulative fashion, meaning each processor
             // has access to the cumulated results of the previous processors.
-            jsonObjs.map( (jsonObj,i) => {
+            arrBuffs.map( (arrBuff,i) => {
+                const data = hook(arrBuff)
                 processingCfg[i].processors.forEach( function(processor){
-                    processor(jsonObj, output)
+                    processor(data, output)
                 })
             } )
             // finally, let the module user do sth with the completely processed output.
